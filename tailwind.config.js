72b6c15b15b5115f3260726cfc3936efe5de5ca4
Jim-Dillon <james.dillon94@gmail.com>
2025-09-07 07:03:12 +0100

    Refine code on first card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  "content": ["./dist/**/*.html", "./src/**/*.css", "./src/**/*.js"],
   theme: {
     extend: {
       "fontFamily": {
